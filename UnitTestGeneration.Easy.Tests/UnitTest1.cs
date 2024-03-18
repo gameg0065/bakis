ea8e934b15086c31e2e402aba250da542c9eca8c
Tomas <gameg0065@gmail.com>
2024-03-18 20:46:19 +0200

    added required projects

---
+++ UnitTestGeneration.Easy.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace UnitTestGeneration.Easy.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ UnitTestGeneration.Moderate.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace UnitTestGeneration.Moderate.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
