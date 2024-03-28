fd120f974ed48de7ab85a4510efe8a9e3b7d8fdc
Tomas <gameg0065@gmail.com>
2024-03-28 18:19:25 +0200

    .net version updated to 8

--- UnitTestGeneration.Easy.App/Program.cs
+++ UnitTestGeneration.Easy.App/Program.cs
@@ -5,13 +5,12 @@ internal abstract class Program
     static void Main(string[] args)
     {
         Console.WriteLine(LoanApplication.FindMonthlyPaymentSize(1000, 1500));
-        Console.WriteLine(LoanApplication.FindMonthlyPaymentSize(-100, 12));
+        Console.WriteLine(LoanApplication.FindMonthlyPaymentSize(100, 12));
     }
 }
 
 public static class LoanApplication
 {
-    // Sveikas teigiamas skaicius
     public static uint FindMonthlyPaymentSize(uint firstPersonSalary, uint secondPersonSalary)
     {
         var sum = (firstPersonSalary + secondPersonSalary) / 10 * 3;
