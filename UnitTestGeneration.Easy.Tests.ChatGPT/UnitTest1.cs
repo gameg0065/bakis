c6056d05fc490d8397f78ebe0048a89bd2935481
Tomas <gameg0065@gmail.com>
2024-03-28 21:50:03 +0200

    Test projects for LLM's

---
+++ UnitTestGeneration.Easy.Tests.ChatGPT/UnitTest1.cs
@@ -0,0 +1,11 @@
+using UnitTestGeneration.Easy.App;
+
+namespace UnitTestGeneration.Easy.Tests.ChatGPT;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ UnitTestGeneration.Easy.Tests.Cloude/UnitTest1.cs
@@ -0,0 +1,11 @@
+using UnitTestGeneration.Easy.App;
+
+namespace UnitTestGeneration.Easy.Tests.Cloude;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
