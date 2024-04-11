fa7b2eddad8cba9efcb214509e24a10beb00e6c7
Tomas <gameg0065@gmail.com>
2024-04-11 23:19:31 +0300

    Added source url

--- UnitTestGeneration.Easy.App/MultiCardsCheck.cs
+++ UnitTestGeneration.Easy.App/MultiCardsCheck.cs
@@ -1,5 +1,7 @@
 namespace UnitTestGeneration.Easy.App;
 
+//  https://github.com/IamREGZ/PokerGame/blob/master/Program.cs
+// cy = 1, co = 0
 public static class MultiCardsCheck
 {
     public static int CheckForMultiCards(string[] valueCounters, int valueCount)
