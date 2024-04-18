8c628e5bc21c83a8fd2eb5467fcce547a8dd09c1
Tomas <gameg0065@gmail.com>
2024-04-19 00:34:37 +0300

    Fixing dotCover bug :)

--- runSonarCube.sh
+++ runSonarCube.sh
@@ -11,6 +11,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Easy.Tests.Gemini.Prompt1" /d:s
 dotnet build UnitTestGeneration.Easy.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Easy.Tests.Gemini.Prompt1/UnitTestGeneration.Easy.Tests.Gemini.Prompt1.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_a253ec8cda16d4d18832a440eb9f182fe401442e"   
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 ### Prompt 2
 
@@ -18,6 +20,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Easy.Tests.Gemini.Prompt2" /d:s
 dotnet build UnitTestGeneration.Easy.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Easy.Tests.Gemini.Prompt2/UnitTestGeneration.Easy.Tests.Gemini.Prompt2.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_a765d3a4b697fc6c8a8e71d556ec4ab97692dcd3"   
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 ### Prompt 3
 
@@ -25,7 +29,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Easy.Tests.Gemini.Prompt3" /d:s
 dotnet build UnitTestGeneration.Easy.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Easy.Tests.Gemini.Prompt3/UnitTestGeneration.Easy.Tests.Gemini.Prompt3.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_93cd14bbf59ffe9011a388bd927741a5d69f3730"   
-
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 # UnitTestGeneration.Moderate.Tests.Gemini.Prompt1
 
@@ -35,6 +40,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Moderate.Tests.Gemini.Prompt1" 
 dotnet build UnitTestGeneration.Moderate.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Moderate.Tests.Gemini.Prompt1/UnitTestGeneration.Moderate.Tests.Gemini.Prompt1.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_9ac82cbf530c045461152223a2fe0fb289170b4f"   
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 ### Prompt 2
 
@@ -42,6 +49,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Moderate.Tests.Gemini.Prompt2" 
 dotnet build UnitTestGeneration.Moderate.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Moderate.Tests.Gemini.Prompt2/UnitTestGeneration.Moderate.Tests.Gemini.Prompt2.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_31d9a1f842bbb57dbefdc33fe8a4b2a766770277"   
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 ### Prompt 3
 
@@ -49,7 +58,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Moderate.Tests.Gemini.Prompt3" 
 dotnet build UnitTestGeneration.Moderate.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Moderate.Tests.Gemini.Prompt3/UnitTestGeneration.Moderate.Tests.Gemini.Prompt3.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_59d0720ce3df31a4faec02fdc93acb6476bdd550"   
-
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 # UnitTestGeneration.Difficult.Tests.Gemini.Prompt1
 
@@ -59,6 +69,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Difficult.Tests.Gemini.Prompt1"
 dotnet build UnitTestGeneration.Difficult.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Difficult.Tests.Gemini.Prompt1/UnitTestGeneration.Difficult.Tests.Gemini.Prompt1.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_097d2984944d2b193afb98155a1db60efb163f2e"   
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 ### Prompt 2
 
@@ -66,6 +78,8 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Difficult.Tests.Gemini.Prompt2"
 dotnet build UnitTestGeneration.Difficult.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Difficult.Tests.Gemini.Prompt2/UnitTestGeneration.Difficult.Tests.Gemini.Prompt2.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_989e94110e3d9905ac9a5956c6ef53ee56c59a22"   
+rm dotCover.Output.html
+rm -rf dotCover.Output
 
 ### Prompt 3
 
@@ -73,4 +87,5 @@ dotnet sonarscanner begin /k:"UnitTestGeneration.Difficult.Tests.Gemini.Prompt3"
 dotnet build UnitTestGeneration.Difficult.App --no-incremental     
 dotnet dotcover test UnitTestGeneration.Difficult.Tests.Gemini.Prompt3/UnitTestGeneration.Difficult.Tests.Gemini.Prompt3.csproj --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.token="sqp_1c5745a6a9995bbcfc67fee5d533f0d27f7d7220"   
-
+rm dotCover.Output.html
+rm -rf dotCover.Output
